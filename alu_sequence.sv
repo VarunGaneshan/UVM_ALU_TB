c39dbd0b55c355e226b87d6ba4a5f538cbc7079e
Varun Ganeshan <94780009+VarunGaneshan@users.noreply.github.com>
2025-08-31 14:56:42 +0530

    Enhance alu_base_sequence with transaction logging
    Updated the body task to include transaction logging and a count variable.

--- alu_sequence.sv
+++ alu_sequence.sv
@@ -7,13 +7,15 @@ class alu_base_sequence extends uvm_sequence #(alu_sequence_item);
   endfunction
 
   virtual task body();
-    repeat(2000) begin
+    int count;
+    repeat(`no_of_trans) begin
       item = alu_sequence_item::type_id::create("item");
       wait_for_grant();
       item.randomize();
-      item.print();
+      `uvm_info(get_type_name(), $sformatf("[%0t] Transaction %0d: cmd=%0d, mode=%0d, inp_valid=%0b, opa=%0d, opb=%0d, cin=%0b, ce=%0b", $time, count, item.cmd, item.mode, item.inp_valid, item.opa, item.opb, item.cin, item.ce), UVM_LOW);
       send_request(item);
       wait_for_item_done();
+      count++;
     end
   endtask
 

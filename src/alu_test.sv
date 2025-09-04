ab976f3780a13ac94a39cb87b7f101d7ecba1738
Varun Ganeshan <94780009+VarunGaneshan@users.noreply.github.com>
2025-09-04 16:54:53 +0530

    Update alu_test.sv

--- src/alu_test.sv
+++ src/alu_test.sv
@@ -1,26 +1,28 @@
-class alu_base_test extends uvm_test;
-  `uvm_component_utils(alu_base_test)
-  alu_env env;
-  alu_base_sequence seq;
-  function new(string name="alu_base_test", uvm_component parent=null);
-    super.new(name, parent);
-  endfunction
+class alu_reg_test extends uvm_test;
+ `uvm_component_utils(alu_reg_test);
+ alu_env env;
+ virtual_sequence v_seq;
 
-  virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    env = alu_env::type_id::create("env", this);
-  endfunction
+ function new(string name="alu_reg_test", uvm_component parent=null);
+   super.new(name, parent);
+ endfunction
 
-  virtual function void end_of_elaboration_phase(uvm_phase phase);
+ virtual function void build_phase(uvm_phase phase);
+   super.build_phase(phase);
+   env = alu_env::type_id::create("env", this);
+ endfunction
+
+ virtual function void end_of_elaboration_phase(uvm_phase phase);
     super.end_of_elaboration_phase(phase);
     uvm_top.print_topology();
-  endfunction
+ endfunction
 
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
     phase.raise_objection(this);
-    seq = alu_base_sequence::type_id::create("seq");
-    seq.start(env.agent.sequencer);
+    v_seq = virtual_sequence::type_id::create("v_seq");
+    v_seq.v_seqr = env.agent.sequencer;
+    v_seq.start(null);
     phase.drop_objection(this);
   endtask
 
